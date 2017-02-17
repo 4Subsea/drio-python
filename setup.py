b9037b1714fbf6cae74efd800b58067f43ae4f91
Ali Cetin <ali.cetin@4subsea.com>
2017-02-17 15:40:40 +0000

    version: bump to v 0.1.4

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 
-setup(name='timeseriesclient', 
-      version='0.1.3',
+setup(name='timeseriesclient',
+      version='0.1.4',
       license='Proprietary',
       description='Python client for timeseries in 4Subsea data reservoir',
       url='http://www.4subsea.com/python/timeseriesclient',
       author='4Subsea',
       author_email='python@4subsea.com',
       packages=find_packages(exclude=['tests', 'integrationtests']),
-        install_requires = [
-            'adal>=0.4.3',
-            'azure>=1.0.3',
-            'azure-storage>=0.30.0',
-            'numpy',
-            'pandas',
-            'requests'
-        ],
-    include_package_data = True,
-    zip_safe=False)
+      install_requires=[
+          'adal>=0.4.3',
+          'azure>=1.0.3',
+          'azure-storage>=0.30.0',
+          'numpy',
+          'pandas',
+          'requests'
+      ],
+      include_package_data=True,
+      zip_safe=False)
