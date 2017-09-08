7fa7406b20083857145b4a4eb91d09d9e4d1632a
Ali Cetin <ali.cetin@4subsea.com>
2017-09-08 13:28:02 +0000

    version: bump up version to v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='datareservoirio',
-      version='0.2.1',
+      version='0.2.2',
       license='Proprietary',
       description='Python client for 4Subsea datareservoir.io',
       url='http://www.4subsea.com/python/datareservoirio',
