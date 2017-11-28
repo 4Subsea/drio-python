b2351d9318c4a78ae191edec4d7b1f576e58fa6b
Peter Lillevold <peter.lillevold@4subsea.com>
2017-11-28 13:49:04 +0000

    fix: support email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='datareservoirio',
       description='Python client for 4Subsea datareservoir.io',
       url='http://www.4subsea.com/python/datareservoirio',
       author='4Subsea',
-      author_email='python@4subsea.com',
+      author_email='support@4subsea.com',
       packages=find_packages(exclude=['tests', 'integrationtests']),
       install_requires=[
           'adal>=0.4.3',
