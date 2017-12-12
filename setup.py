1e8b13b5bf3c994cd9f01601526f5dbf4d224f7b
Peter Lillevold <peter.lillevold@4subsea.com>
2017-12-12 14:33:27 +0000

    wip: set lib version in setup.py before deploy - case 16306

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 
 setup(name='datareservoirio',
-      version='0.2.3',
+      version='0.0.1',
       license='Proprietary',
       description='Python client for 4Subsea datareservoir.io',
       url='http://www.4subsea.com/python/datareservoirio',
       author='4Subsea',
       author_email='support@4subsea.com',
-      packages=find_packages(exclude=['tests', 'integrationtests']),
+      packages=find_packages(exclude=['tests', 'integrationtests', 'packages']),
       install_requires=[
           'adal>=0.4.3',
           'azure-storage>=0.30.0',
