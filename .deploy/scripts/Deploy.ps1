f025c4b48fb083ba20d4a00f1df62e3e068c3118
Peter Lillevold <peter.lillevold@4subsea.com>
2019-01-07 13:08:04 +0000

    feat: upload python wheel to test or prod feed at pypi.org - case 20435

--- .deploy/scripts/Deploy.ps1
+++ .deploy/scripts/Deploy.ps1
@@ -10,8 +10,12 @@ if (!(Test-Path variable:global:OctopusReleaseNumber)) {
     Write-Error "OctopusReleaseNumber variable must be set"
     return
 }
-if (!(Test-Path variable:global:PyPiLocation)) {
-    Write-Error "PyPiLocation variable must be set"
+if (!(Test-Path variable:global:PyPiUser)) {
+    Write-Error "PyPiUser variable must be set"
+    return
+}
+if (!(Test-Path variable:global:PyPiPassword)) {
+    Write-Error "PyPiUser variable must be set"
     return
 }
 if (!(Test-Path variable:global:DocLocation)) {
@@ -20,11 +24,23 @@ if (!(Test-Path variable:global:DocLocation)) {
 }
 
 $Root = (Get-ScriptDirectory)
-$PythonSource = Join-Path $Root "*.whl"
 $PythonDocSource = Join-Path $Root "docs"
 
-Write-Host "Copying Python library from $PythonSource to $PyPiLocation"
-& "xcopy" "$PythonSource" "$PyPiLocation" /Y /I
+# Create/activate python environment to work with
+Write-Host "Creating/updating deployment environment '$PythonEnvironment'"
+& "conda env update -f deployenv.yml"
+Write-Host "Activating deployment environment '$PythonEnvironment'"
+& "activate $PythonEnvironment"
+
+if ($PyPiRepositoryUrl) {
+    # Upload to specific repository
+    Write-Host "Uploading to $PyPiRepositoryUrl"
+    & "twine upload --repository-url $PyPiRepositoryUrl * -u $PyPiUser -p $PyPiPassword"
+}
+else {
+    Write-Host "Uploading to default repository"
+    & "twine upload * -u $PyPiUser -p $PyPiPassword"
+}
 
 Write-Host "Copying Python documentation from $PythonDocSource to $DocLocation"
 & "$AzCopyTool" /Z:azjournal /V:azlog.txt /Source:"$PythonDocSource" /Dest:"$DocLocation" /DestKey:"$DocLocationKey" /S /Y /SetContentType
