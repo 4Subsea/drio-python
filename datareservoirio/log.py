ed14dea0c18e145550067f13b7a0090790ebbfbc
Peter Lillevold <peter.lillevold@4subsea.com>
2021-01-05 15:57:18 +0100

    Simplify log.py
    Rely on the existing support for formatting time and level into the message in the logging framework

--- datareservoirio/log.py
+++ datareservoirio/log.py
@@ -1,38 +1,31 @@
-from datetime import datetime
-
-
 class LogWriter(object):
     def __init__(self, logger):
         self.logger = logger
 
     def critical(self, msg, origin=None):
-        msg = self.format("CRITICAL", msg, origin)
+        msg = self.format(msg, origin)
         return self.logger.critical(msg)
 
     def error(self, msg, origin=None):
-        msg = self.format("ERROR", msg, origin)
+        msg = self.format(msg, origin)
         return self.logger.error(msg)
 
     def warning(self, msg, origin=None):
-        msg = self.format("WARNING", msg, origin)
+        msg = self.format(msg, origin)
         return self.logger.warning(msg)
 
     def info(self, msg, origin=None):
-        msg = self.format("INFO", msg, origin)
+        msg = self.format(msg, origin)
         return self.logger.info(msg)
 
     def debug(self, msg, origin=None):
-        msg = self.format("DEBUG", msg, origin)
+        msg = self.format(msg, origin)
         return self.logger.debug(msg)
 
-    def format(self, lvl, msg, origin=None):
-        time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
-
+    def format(self, msg, origin=None):
         if origin:
-            origin = "." + origin
+            origin = f"{self.logger.name}.{origin} "
         else:
             origin = ""
 
-        base = "{} *** {} *** {}{} *** {}"
-
-        return base.format(time, lvl, self.logger.name, origin, msg)
+        return f"{origin}{msg}"
