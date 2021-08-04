091d99abf56b41e95e1b183c7af22ed698464bea
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2021-08-04 10:24:37 +0200

    enh: simplified AzureBlobClient

--- datareservoirio/storage/storage_engine.py
+++ datareservoirio/storage/storage_engine.py
@@ -2,7 +2,58 @@ import numpy as np
 import pandas as pd
 
 from azure.storage.blob import BlobClient
-from io import BytesIO, TextIOWrapper
+from io import BytesIO, TextIOWrapper, StringIO
+
+
+# class _AzureBlobClient(BlobClient):
+#     def __init__(self, params):
+#         self._account = params["Account"]
+#         self._sas_key = params["SasKey"]
+#         self._container_name = params["Container"]
+#         self._blob_name = params["Path"]
+#         self._account_url = f"https://{self._account}.blob.core.windows.net"
+
+#         super(AzureBlobClient, self).__init__(
+#             self._account_url, self._container_name, self._blob_name, credential=self._sas_key
+#         )
+
+#     def get_blob_to_df(self):
+#         with BytesIO() as binary_content:
+#             self.download_blob().readinto(binary_content)
+
+#             binary_content.seek(0)
+#             with TextIOWrapper(binary_content, encoding="utf-8") as text_content:
+#                 values_raw = np.asarray(
+#                     list(map(self._split_value, text_content)), dtype="O"
+#                 )
+
+#         index = values_raw.T[0].astype("int64")
+#         values = values_raw.T[1]
+#         try:
+#             dtype = "float64"
+#             values = values.astype(dtype)
+#         except ValueError:  # unable to cast to float
+#             # awaiting Pandas 1.0 string type + pd.NA
+#             dtype = "O"
+#         finally:
+#             df = pd.DataFrame(values, index, columns=["values"], dtype=dtype)
+
+#         return df
+
+#     def _split_value(self, line):
+#         val0, val1 = line.rstrip().split(",", 1)
+#         if not val1:
+#             val1 = None
+#         return val0, val1
+
+#     def create_blob_from_series(self, series):
+
+#         data = series.copy()
+#         data.index = data.index.view("int64")
+
+#         block_data = data.to_csv(header=False, line_terminator="\n")
+
+#         self.upload_blob(block_data.encode("ascii"), blob_type="BlockBlob")
 
 
 class AzureBlobClient(BlobClient):
@@ -14,37 +65,26 @@ class AzureBlobClient(BlobClient):
         self._account_url = f"https://{self._account}.blob.core.windows.net"
 
         super(AzureBlobClient, self).__init__(
-            self._account_url, self._container_name, self._blob_name, credential=self._sas_key
+            self._account_url,
+            self._container_name,
+            self._blob_name,
+            credential=self._sas_key,
         )
 
     def get_blob_to_df(self):
-        with BytesIO() as binary_content:
-            self.download_blob().readinto(binary_content)
+        text_content = self.download_blob().readall().decode(encoding="utf-8")
 
-            binary_content.seek(0)
-            with TextIOWrapper(binary_content, encoding="utf-8") as text_content:
-                values_raw = np.asarray(
-                    list(map(self._split_value, text_content)), dtype="O"
-                )
-
-        index = values_raw.T[0].astype("int64")
-        values = values_raw.T[1]
-        try:
-            dtype = "float64"
-            values = values.astype(dtype)
-        except ValueError:  # unable to cast to float
-            # awaiting Pandas 1.0 string type + pd.NA
-            dtype = "O"
-        finally:
-            df = pd.DataFrame(values, index, columns=["values"], dtype=dtype)
+        df = pd.read_csv(
+            StringIO(text_content),
+            sep=",",
+            index_col=0,
+            header=None,
+            names=(None, "values"),
+            dtype={"values": "float64"},
+        )
+        df.index = df.index.astype("int64")
 
         return df
-
-    def _split_value(self, line):
-        val0, val1 = line.rstrip().split(",", 1)
-        if not val1:
-            val1 = None
-        return val0, val1
 
     def create_blob_from_series(self, series):
 
