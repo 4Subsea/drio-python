dfc852d7bff3eac75f5608d269be2cf18f525c3a
anna-follestad-4ss <aff@4subsea.com>
2022-10-12 09:01:52 +0200

    docs: include relbar

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ intersphinx_mapping = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates/autosummary"]
+templates_path = ["_templates/autosummary", "_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
