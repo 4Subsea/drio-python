e9e050277e12fd3f70d6d528774c987863242453
anna-follestad-4ss <aff@4subsea.com>
2022-10-13 10:29:41 +0200

    docs: fixed external links

--- docs/conf.py
+++ docs/conf.py
@@ -87,12 +87,12 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/4subsea/fourinsight-xyz",
+            "url": "https://github.com/4Subsea/drio-python",
             "icon": "fab fa-github",
         },
         {
             "name": "PyPI",
-            "url": "https://pypi.org/project/fourinsight-xyz",
+            "url": "https://pypi.org/project/datareservoirio/",
             "icon": "fas fa-box",
         },
     ],
