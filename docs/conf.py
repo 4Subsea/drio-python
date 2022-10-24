5fbf29469284efc817fb607a839a6f1c6cde0a1c
anna-follestad-4ss <72202484+anna-follestad-4ss@users.noreply.github.com>
2022-10-24 09:50:43 +0200

    docs/fix: external links in documentation

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ intersphinx_mapping = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates/autosummary"]
+templates_path = ["_templates/autosummary", "_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -87,12 +87,12 @@ html_theme_options = {
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/4subsea/fourinsight-xyz",
+            "url": "https://github.com/4Subsea/drio-python",
             "icon": "fab fa-github",
         },
         {
             "name": "PyPI",
-            "url": "https://pypi.org/project/fourinsight-xyz",
+            "url": "https://pypi.org/project/datareservoirio/",
             "icon": "fas fa-box",
         },
     ],
