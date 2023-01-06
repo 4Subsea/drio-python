ab8d3cdadab8d64f3a64ec03e7553dae6852bd5f
Anna Fæhn Follestad <anna.fehn.follestad@4subsea.com>
2023-01-06 10:45:45 +0100

    refactor: store secrets details in env

--- tests_integration/_auth.py
+++ tests_integration/_auth.py
@@ -1,3 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+#class CLIENT:
+#    CLIENT_ID = "1668de40-c3c0-452d-8862-b403e64ffc5b"
+#    CLIENT_SECRET = "3H51_b-CH5nL3v.-dfHB2h0IxjPZG8emx."
+
 class CLIENT:
-    CLIENT_ID = "1668de40-c3c0-452d-8862-b403e64ffc5b"
-    CLIENT_SECRET = "3H51_b-CH5nL3v.-dfHB2h0IxjPZG8emx."
+    CLIENT_ID = os.environ.get("client_id")
+    CLIENT_SECRET = os.environ.get("client_secret")
