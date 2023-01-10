2e3f6f75c70f7ebed46e617a20dd1ad549012384
Anna Fæhn Follestad <anna.fehn.follestad@4subsea.com>
2023-01-10 16:40:14 +0100

    dep: remove deprecated session

--- tests_integration/test_storage/test_basedownloader_filecachedownload.py
+++ tests_integration/test_storage/test_basedownloader_filecachedownload.py
@@ -3,11 +3,13 @@ import unittest
 from functools import partial
 from timeit import timeit
 
-import requests
+import pandas as pd
+import numpy as np
+
 
 from datareservoirio.authenticate import ClientAuthenticator
-from datareservoirio.rest_api import TimeSeriesAPI
-from datareservoirio.storage import BaseDownloader, FileCacheDownload
+from datareservoirio.rest_api import TimeSeriesAPI, FilesAPI
+from datareservoirio.storage import BaseDownloader, FileCacheDownload, DirectUpload
 from tests_integration._auth import CLIENT
 
 log = logging.getLogger(__file__)
@@ -20,22 +22,47 @@ class Test_CachedDownloadStrategy(unittest.TestCase):
         self.timeseries_api = TimeSeriesAPI(session=self.auth)
         self._cache_root = "./_cache/test_filecachedownload"
 
-        self._session = requests.Session()
+        # Set up some stuff here
+        self.files_api = FilesAPI(session=self.auth)
+
+        uploader = DirectUpload()
+
+        df = pd.DataFrame({"values": np.arange(100.0)}, index=np.arange(0, 100))
+
+        upload_params = self.files_api.upload()
+        self.token_fileid = upload_params["FileId"]
+
+        uploader.put(upload_params, df)
+
+        self.files_api.commit(upload_params["FileId"])
+
+
+
+
+
 
     def tearDown(self):
         self.auth.close()
-        self._session.close()
 
     def test_get_with_parquet_format(self):
         strategy = BaseDownloader(
             FileCacheDownload(
-                cache_root=self._cache_root, format_="parquet", session=self._session
+                cache_root=self._cache_root, format_="parquet"
             )
         )
 
+        response = self.timeseries_api.create()
+        myfileid = self.token_fileid
+
+        
+        response = self.timeseries_api.add(response["TimeSeriesId"], myfileid)
+
+
         chunks = self.timeseries_api.download_days(
-            TIMESERIESID, 1513468800000000000, 1513814500000000000
+            response["TimeSeriesId"], 0,100
         )
+
+
         iterations = 100
 
         usedtime = timeit(stmt=partial(strategy.get, chunks), number=iterations)
@@ -45,12 +72,19 @@ class Test_CachedDownloadStrategy(unittest.TestCase):
     def test_get_with_csv_format(self):
         strategy = BaseDownloader(
             FileCacheDownload(
-                cache_root=self._cache_root, format_="csv", session=self._session
+                cache_root=self._cache_root, format_="csv"
             )
         )
 
+        response = self.timeseries_api.create()
+        myfileid = self.token_fileid
+
+        
+        response = self.timeseries_api.add(response["TimeSeriesId"], myfileid)
+
+
         chunks = self.timeseries_api.download_days(
-            TIMESERIESID, 1513468800000000000, 1513814500000000000
+            response["TimeSeriesId"], 0,100
         )
         iterations = 100
 
