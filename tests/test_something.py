06d760eea5ddcb20360425407a8e51b79e7b72cc
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-01 16:07:35 +0100

    delete obsolete test

--- tests/test_something.py
+++ tests/test_something.py
@@ -42,30 +42,3 @@ def test_one_more_thing(mock_requests_get):
         dict_expect = json.load(f)
 
     assert dict_out == dict_expect
-
-
-class Test__blob_to_df:
-    @pytest.fixture
-    def mock_response_get(self, monkeypatch, get_response):
-        def mock_get(*args, **kwargs):
-            content_path = TEST_PATH / "testdata" / "example_drio_blob_file.csv"
-            get_response._content_path = content_path
-            return get_response
-
-        monkeypatch.setattr(requests, "get", mock_get)
-
-    @pytest.fixture
-    def df_expect(self):
-        df = pd.read_csv(
-            TEST_PATH / "testdata" / "example_drio_blob_file.csv",
-            header=None,
-            names=("index", "values"),
-            dtype={"index": "int64", "values": "str"},
-            encoding="utf-8",
-        ).astype({"values": "float64"}, errors="ignore")
-
-        return df
-
-    def test__blob_to_df(self, mock_response_get, df_expect):
-        df_out = drio.storage.storage._blob_to_df("some/url")
-        pd.testing.assert_frame_equal(df_out, df_expect)
