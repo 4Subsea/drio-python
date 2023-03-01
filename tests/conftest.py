8c72683939a986b3ac38b27d32f1652b673bc8d0
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-01 13:51:34 +0100

    some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import json
 
 import requests
 
+
 @pytest.fixture
 def request_response():
     response = Mock()
@@ -21,9 +22,10 @@ def request_response():
 
 
 @pytest.fixture
-def request_response2():
-    class ResponseFactory(Mock):
+def get_response():
+    class ResponseFactory:
         def __init__(self):
+            self._stream = False
             self._raise_for_status = False
             self._content_path = None
             super().__init__()
@@ -35,9 +37,16 @@ def request_response2():
 
         @property
         def content(self):
-            with open(self._content_path, model="rb") as f:
+            with open(self._content_path, mode="rb") as f:
                 content = f.read()
             return content
+
+        def iter_content(self, chunk_size=1, decode_unicode=False):
+            with open(self._content_path, mode="rb") as f:
+                content_iter = [
+                    f.read(chunk_size).decode() if decode_unicode else f.read(chunk_size)
+                ]
+            return content_iter
 
         def json(self, **kwargs):
             return json.loads(self.content, **kwargs)
