773723b3a7f5b5ecd694caaca21320b56c19c216
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-03 14:54:56 +0100

    test raise_for_status

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -1,5 +1,7 @@
+import pytest
 from pathlib import Path
 import pandas as pd
+import requests
 
 import datareservoirio as drio
 
@@ -8,6 +10,10 @@ TEST_PATH = Path(__file__).parent
 
 
 class Test__blob_to_df:
+    """
+    Tests the ``_blob_to_df`` function.
+    """
+
     def test__blob_to_df(self, mock_requests_get):
         blob_url = "example/drio/blob/file"
         df_out = drio.storage.storage._blob_to_df(blob_url)
@@ -22,3 +28,18 @@ class Test__blob_to_df:
         ).astype({"values": "float64"}, errors="ignore")
 
         pd.testing.assert_frame_equal(df_out, df_expect)
+
+    def test_raise_for_status(self, monkeypatch):
+        """Tests if ``raise_for_status`` is called"""
+        class MockResponse:
+            def raise_for_status(self):
+                raise requests.HTTPError
+
+        def mock_get(*args, **kwargs):
+            return MockResponse()
+
+        monkeypatch.setattr(requests, "get", mock_get)
+
+        with pytest.raises(requests.HTTPError):
+            blob_url = "example/drio/blob/file"
+            _ = drio.storage.storage._blob_to_df(blob_url)
