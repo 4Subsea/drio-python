6abb876936f7febfe017eb63e1a9481fd58bc9b1
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-03 15:31:46 +0100

    some changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,21 +10,27 @@ TEST_PATH = Path(__file__).parent
 URI_RESPONSE_MAP = {
     "example/drio/blob/file": {
         "content_path": str(TEST_PATH / "testdata" / "example_drio_blob_file.csv"),
-    }
+        "raise_for_status": False,
+    },
+    "example/no/exist": {
+        "content_path": None,
+        "raise_for_status": True,
+    },
 }
 
 
-def uri_to_file(uri):
+def uri_to_config(uri):
+    """Get response mock configuration from ``uri``"""
     if uri not in URI_RESPONSE_MAP:
         raise ValueError
-    return URI_RESPONSE_MAP[uri]["content_path"]
+    return URI_RESPONSE_MAP[uri]
 
 
 class MockGetResponse:
-    def __init__(self, content_path=None, stream=False, **kwargs):
+    def __init__(self, content_path=None, raise_for_status=False, stream=False, **kwargs):
         self._content_path = content_path
         self._stream = stream
-        self._raise_for_status = False
+        self._raise_for_status = raise_for_status
 
     def raise_for_status(self):
         if self._raise_for_status:
@@ -54,7 +60,7 @@ class MockGetResponse:
 @pytest.fixture
 def mock_requests_get(monkeypatch):
     def mock_get(url, **kwargs):
-        file_path = uri_to_file(url)
-        return MockGetResponse(content_path=file_path, **kwargs)
+        config = uri_to_config(url)
+        return MockGetResponse(**config, **kwargs)
 
     monkeypatch.setattr(requests, "get", mock_get)
