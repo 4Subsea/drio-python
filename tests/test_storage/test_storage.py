c622b61ee167f3a4b29a5a7b70316d1f43df4157
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-06 13:50:08 +0100

    add test

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -1,4 +1,6 @@
+from hashlib import md5
 from pathlib import Path
+from unittest.mock import ANY, Mock, patch
 
 import pandas as pd
 import pytest
@@ -37,3 +39,34 @@ class Test__blob_to_df:
         with pytest.raises(requests.HTTPError):
             blob_url = "example/no/exist"
             _ = drio.storage.storage._blob_to_df(blob_url)
+
+
+class Test__df_to_blob:
+    """
+    Tests the :func:`_df_to_blob` function.
+    """
+
+    def test__df_to_blob(self, tmp_path):
+        """Tests ``_df_to_blob`` function."""
+
+        csv_file = TEST_PATH.parent / "testdata" / "example_drio_blob_file.csv"
+        df = pd.read_csv(
+            csv_file,
+            header=None,
+            names=("index", "values"),
+            dtype={"index": "int64", "values": "str"},
+            encoding="utf-8",
+        ).astype({"values": "float64"}, errors="ignore")
+
+        mock_response = Mock()
+
+        with patch.object(
+            requests, "put", side_effect=lambda *args, **kwargs: mock_response
+        ) as mock_put:
+            blob_url = "example/blob/endpoint"
+            _ = drio.storage.storage._df_to_blob(df, blob_url)
+
+            mock_put.assert_called_once_with(
+                blob_url, headers={"x-ms-blob-type": "BlockBlob"}, data=ANY
+            )
+            mock_response.raise_for_status.assert_called_once()
