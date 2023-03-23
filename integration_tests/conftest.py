55c1c5e957cd4cb0555ce6cf165b02e2cec95530
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-03-23 15:19:55 +0100

    some inprovements

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -14,7 +14,7 @@ def store_created(monkeypatch, series_created):
     class ClientStoreCreated(drio.Client):
         def create(self, *args, **kwargs):
             return_value = super().create(*args, **kwargs)
-            series_id = kwargs.get("series_id") or args[0]
+            series_id = return_value["TimeSeriesId"]
             series_created.add(series_id)
             return return_value
 
@@ -22,16 +22,22 @@ def store_created(monkeypatch, series_created):
 
 
 @pytest.fixture
-def client(store_created, series_created):
+def auth_session():
     client_id = "0ee5d1b4-2271-4595-8c23-f2361d713a47"
     client_secret = "2iF8Q~t_WniDYJmFnM8cjB6KoBGH-2C9Imij9cKa"
-    client = drio.authenticate.ClientAuthenticator(client_id, client_secret)
+    return drio.authenticate.ClientAuthenticator(client_id, client_secret)
+
+
+@pytest.fixture
+def client(auth_session, store_created, series_created):
+
+    client = drio.Client(auth_session)
+
     yield client
 
-    for series_id_i in series_created:
+    while series_created:
+        series_id_i = series_created.pop()
         try:
             client.delete(series_id_i)
         except HTTPError:
             pass
-        finally:
-            series_created.remove(series_id_i)
--- integration_tests/test_numeric_datetime.py
+++ integration_tests/test_numeric_datetime.py
@@ -7,8 +7,42 @@ Tests the following:
     * Delete the timeseries from DataReservoi.io
 
 """
+import numpy as np
+import pandas as pd
 import datareservoirio as drio
 
 
 def test_numeric_datetime(client):
-    assert 1 == 1
+
+    # Set environment to 'TEST'
+    # -------------------------
+    env = drio.environments.Environment()
+    env.set_test()
+
+    # Create some dummy data
+    # ----------------------
+
+    # First 'chunk' of data
+    start_a = "2022-12-28 00:00"
+    end_a = "2023-01-02 00:00"
+    freq_a = pd.to_timedelta(0.1, "s")
+
+    timestamps_a = pd.date_range(start_a, end_a, freq=freq_a, tz="utc", inclusive="left")
+    values_a = np.random.random(len(timestamps_a))
+
+    series_a = pd.Series(data=values_a, index=timestamps_a, name="values")
+
+    # Second 'chunk 'of data
+    start_b = "2023-01-02 00:00"
+    end_b = "2023-01-02 03:00"
+    freq_b = pd.to_timedelta(0.1, "s")
+
+    timestamps_b = pd.date_range(start_b, end_b, freq=freq_b, tz="utc", inclusive="left")
+    values_b = np.random.random(len(timestamps_b))
+
+    series_b = pd.Series(data=values_b, index=timestamps_b, name="values")
+
+    # Create and upload timeseries to DRIO
+    # ------------------------------------
+    response_create = client.create(series=series_a, wait_on_verification=True)
+    series_id = response_create["TimeSeriesId"]
