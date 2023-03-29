e1d26a424c579c6d26f454bef272f9b4b6945322
Ali Amin Ahmad <ali.ahmad@4subsea.com>
2023-03-29 14:25:50 +0200

    code formatting

--- datareservoirio/client.py
+++ datareservoirio/client.py
@@ -250,8 +250,8 @@ class Client:
             result = func(self, series_id, start=start, end=end, **kwargs)
             end_time = time.perf_counter()
             elapsed_time = end_time - start_time
-            start_date_as_str=None
-            end_date_as_str=None
+            start_date_as_str = None
+            end_date_as_str = None
             if start:
                 start_date_as_str = pd.to_datetime(
                     start, dayfirst=True, unit="ns", utc=True
