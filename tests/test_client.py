5a0b61d5d99501412344b323f209089a624cb1b4
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-04-11 14:44:45 +0200

    test get with start/end as datetime

--- tests/test_client.py
+++ tests/test_client.py
@@ -94,3 +94,27 @@ class Test_Client:
         series_expect.index = pd.to_datetime(series_expect.index, utc=True)
 
         pd.testing.assert_series_equal(series_out, series_expect)
+
+    def test_get_start_end_as_datetime(self, client):
+        start = pd.to_datetime("2022-12-30T00:00:00+00:00")
+        end = pd.to_datetime("2023-01-02T23:59:00+00:00")
+        series_out = client.get(
+            "2fee7f8a-664a-41c9-9b71-25090517c275",
+            start=start,
+            end=end,
+            convert_date=True
+        )
+
+        df_expect = pd.read_csv(
+            TEST_PATH / "testdata" / "RESPONSE_GROUP1" / "dataframe.csv",
+            header=None,
+            names=("index", "values"),
+            dtype={"index": "int64", "values": "float64"},
+            encoding="utf-8",
+        )
+
+        series_expect = df_expect.set_index("index").squeeze("columns")
+        series_expect.index.name = None
+        series_expect.index = pd.to_datetime(series_expect.index, utc=True)
+
+        pd.testing.assert_series_equal(series_out, series_expect)
