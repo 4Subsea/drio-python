8e000d93bd0d0153d338e3f9b37a97ad4d3fbd35
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-04-11 13:35:53 +0200

    started on testing get

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,10 @@
+from pathlib import Path
 import pytest
+import pandas as pd
 import datareservoirio as drio
+
+
+TEST_PATH = Path(__file__).parent
 
 
 class Test_Client:
@@ -7,7 +12,7 @@ class Test_Client:
     Tests the ``datareservoirio.Client`` class.
     """
     @pytest.fixture
-    def client_no_cache(self, auth_session):
+    def client(self, auth_session):
         return drio.Client(auth_session, cache=False)
 
     def test__init__(self, auth_session):
@@ -19,5 +24,19 @@ class Test_Client:
         assert isinstance(client._metadata_api, drio.rest_api.MetadataAPI)
         assert isinstance(client._storage, drio.storage.Storage)
 
-    def test_get(self, client_no_cache):
-        pass
+    def test_get(self, client):
+        series_out = client.get(
+            "2fee7f8a-664a-41c9-9b71-25090517c275",
+            start=1672358400000000000,
+            end=1672703940000000000
+        )
+
+        df_expect = pd.read_csv(
+            TEST_PATH.parent / "testdata" / "RESPONSE_GROUP1" / "dataframe.csv",
+            header=None,
+            names=("index", "values"),
+            dtype={"index": "int64", "values": "float64"},
+            encoding="utf-8",
+        )
+
+        pd.testing.assert_frame_equal(series_out, df_expect["values"])
