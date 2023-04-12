69c9b4048707d5a994407b2c88b30dbb859e1412
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-04-12 14:41:13 +0200

    some changes

--- integration_tests/test_numeric_cached.py
+++ integration_tests/test_numeric_cached.py
@@ -100,13 +100,11 @@ def test_numeric_cached(cleanup_series, tmp_path):
 
     # Check that data is read from cache
     time_before_get = time.time()
-    time.sleep(1)
     _ = client.get(series_id, start=None, end=None)
-    time.sleep(1)
     time_after_get = time.time()
     for cache_file_i in CACHE_PATH.iterdir():
-        time_file_read_i = os.path.getatime(cache_file_i)
-        assert time_before_get < time_file_read_i < time_after_get
+        time_access_file_i = os.path.getatime(cache_file_i)   # last access time
+        assert time_before_get < time_access_file_i < time_after_get
 
     # Delete timeseries from DataReservoir.io
     client.delete(series_id)
