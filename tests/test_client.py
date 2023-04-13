52dc65601aaf56f658f8dc6dc0419308f65dea06
Ali Cetin <ali.cetin@4subsea.com>
2023-04-13 10:48:24 +0200

    remove some assertions

--- tests/test_client.py
+++ tests/test_client.py
@@ -26,13 +26,7 @@ class Test_Client:
         return drio.Client(auth_session, cache=False)
 
     def test__init__(self, auth_session):
-        client = drio.Client(auth_session, cache=False)
-
-        assert client._auth_session is auth_session
-        assert isinstance(client._timeseries_api, drio.rest_api.TimeSeriesAPI)
-        assert isinstance(client._files_api, drio.rest_api.FilesAPI)
-        assert isinstance(client._metadata_api, drio.rest_api.MetadataAPI)
-        assert isinstance(client._storage, drio.storage.Storage)
+        drio.Client(auth_session, cache=False)
 
     def test_get(self, client):
         start = 1672358400000000000
