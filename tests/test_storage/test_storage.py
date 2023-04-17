b36ffdaa3cd13ed7c7a2597b538f2a00a66b12d9
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-04-17 15:20:37 +0200

    fix two tests

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -147,8 +147,31 @@ class Test_Storage:
         assert storage._session is auth_session
 
     def test_get(self, storage_no_cache):
-        target_url = "https://reservoir-api.4subsea.net/api/timeseries/2fee7f8a-664a-41c9-9b71-25090517c275/data/days?start=1672358400000000000&end=1672703939999999999"
-        df_out = storage_no_cache.get(target_url)
+
+        blob_sequence = (
+            {
+                "Path": "1b0d906b34ce40d69520e46f49a54545/2022/12/30/day/csv/19356.csv",
+                "Endpoint": "https://permanentprodu000p169.blob.core.windows.net/data/1b0d906b34ce40d69520e46f49a54545/2022/12/30/day/csv/19356.csv?versionid=2023-03-14T14:56:10.8583280Z&skoid=4b73ab81-cb6b-4de8-934e-cf62e1cc3aa2&sktid=cdf4cf3d-de23-49cf-a9b0-abd2b675f253&skt=2023-03-14T13%3A50%3A58Z&ske=2023-03-15T13%3A50%3A57Z&sks=b&skv=2021-10-04&sv=2021-10-04&spr=https&se=2023-03-14T17%3A21%3A07Z&sr=b&sp=r&sig=QTYi%2FAeiMFg72EyxC8d%2BV0M0lmgbYek%2BfGXhAXvme1U%3D",
+                "ContentMd5": "reFOcOgW3qct5b0VJ/5g7g==",
+            },
+            {
+                "Path": "1b0d906b34ce40d69520e46f49a54545/2022/12/31/day/csv/19357.csv",
+                "Endpoint": "https://permanentprodu003p208.blob.core.windows.net/data/1b0d906b34ce40d69520e46f49a54545/2022/12/31/day/csv/19357.csv?versionid=2023-03-14T14:56:11.0377879Z&skoid=4b73ab81-cb6b-4de8-934e-cf62e1cc3aa2&sktid=cdf4cf3d-de23-49cf-a9b0-abd2b675f253&skt=2023-03-14T14%3A00%3A24Z&ske=2023-03-15T14%3A00%3A24Z&sks=b&skv=2021-10-04&sv=2021-10-04&spr=https&se=2023-03-14T17%3A21%3A07Z&sr=b&sp=r&sig=MIchFxo2gfRsa82kqTgHtq1DRY7cQldsZ0jQi4ySPZE%3D",
+                "ContentMd5": "7ZeXRmZnw057D3QxWIYWmQ==",
+            },
+            {
+                "Path": "1b0d906b34ce40d69520e46f49a54545/2023/01/01/day/csv/19358.csv",
+                "Endpoint": "https://permanentprodu003p153.blob.core.windows.net/data/1b0d906b34ce40d69520e46f49a54545/2023/01/01/day/csv/19358.csv?versionid=2023-03-14T14:56:11.1047474Z&skoid=4b73ab81-cb6b-4de8-934e-cf62e1cc3aa2&sktid=cdf4cf3d-de23-49cf-a9b0-abd2b675f253&skt=2023-03-14T13%3A44%3A11Z&ske=2023-03-15T13%3A43%3A49Z&sks=b&skv=2021-10-04&sv=2021-10-04&spr=https&se=2023-03-14T17%3A21%3A07Z&sr=b&sp=r&sig=Uo5EqCiYRXcWDjifsiom9uGi7CNhFkGZQ4lBsGgEmC8%3D",
+                "ContentMd5": "L96VxzU07mh5lcEsbuul9g==",
+            },
+            {
+                "Path": "1b0d906b34ce40d69520e46f49a54545/2023/01/02/day/csv/19359.csv",
+                "Endpoint": "https://permanentprodu002p192.blob.core.windows.net/data/1b0d906b34ce40d69520e46f49a54545/2023/01/02/day/csv/19359.csv?versionid=2023-03-14T14:56:11.1906071Z&skoid=4b73ab81-cb6b-4de8-934e-cf62e1cc3aa2&sktid=cdf4cf3d-de23-49cf-a9b0-abd2b675f253&skt=2023-03-14T13%3A50%3A18Z&ske=2023-03-15T13%3A50%3A18Z&sks=b&skv=2021-10-04&sv=2021-10-04&spr=https&se=2023-03-14T17%3A21%3A07Z&sr=b&sp=r&sig=bto08GDGsK7M%2FZLXOQ%2Bhm3sgYd%2B23g6rs5fI0nCq9AQ%3D",
+                "ContentMd5": "CYS6gLUWLhZO6fFcGdbItg==",
+            }
+        )
+
+        df_out = storage_no_cache.get(blob_sequence)
 
         df_expect = DataHandler.from_csv(
             TEST_PATH.parent / "testdata" / "RESPONSE_GROUP1" / "dataframe.csv",
@@ -167,8 +190,8 @@ class Test_Storage:
         pd.testing.assert_frame_equal(df_out, df_expect)
 
     def test_get_empty(self, storage_no_cache):
-        target_url = "https://reservoir-api.4subsea.net/api/timeseries/e3d82cda-4737-4af9-8d17-d9dfda8703d0/data/days"
-        df_out = storage_no_cache.get(target_url)
+        blob_sequence = []
+        df_out = storage_no_cache.get(blob_sequence)
 
         df_expect = pd.DataFrame(columns=("index", "values")).astype({"index": "int64"})
 
