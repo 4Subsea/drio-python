361bafe13f641a0fe4d294b65a93ec43785b4528
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-04-26 11:35:53 +0200

    refactor tests

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -526,32 +526,10 @@ class Test_Storage:
 
 
 class Test_StorageCache:
-    @pytest.fixture
-    def cache_root_empty(self, tmp_path):
-        """Temporary cache root (empty)"""
-        return tmp_path / ".cache"
-
-    @pytest.fixture
-    def storage_cache_empty(self, cache_root_empty):
-        """``StorageCache`` instance (empty cache)"""
-        storage_cache = StorageCache(
-            max_size=1024,
-            cache_root=cache_root_empty,
-        )
-        return storage_cache
-
     # @pytest.fixture
-    # def cache_root(self, tmp_path, STOREFORMATVERSION):
-    #     """Temporary cache root (with data)"""
-    #     dst_cache_root = tmp_path
-    #     dst_cache_path = dst_cache_root / STOREFORMATVERSION
-    #     dst_cache_path.mkdir()
-    #     src_cache_path = (
-    #         TEST_PATH.parent / "testdata" / "RESPONSE_GROUP2" / "cache" / "v3"
-    #     )
-    #     for src_cache_file_i in src_cache_path.iterdir():
-    #         shutil.copyfile(src_cache_file_i, dst_cache_path / src_cache_file_i.name)
-    #     return dst_cache_root
+    # def cache_root_empty(self, tmp_path):
+    #     """Temporary cache root (empty)"""
+    #     return tmp_path / ".cache"
 
     @pytest.fixture
     def cache_root(self, tmp_path):
@@ -570,6 +548,15 @@ class Test_StorageCache:
                 shutil.copyfile(src_file_i, dst / src_file_i.name)
 
         return fill_cache
+
+    @pytest.fixture
+    def storage_cache_empty(self, cache_root):
+        """``StorageCache`` instance (empty cache)"""
+        storage_cache = StorageCache(
+            max_size=1024,
+            cache_root=cache_root,
+        )
+        return storage_cache
 
     @pytest.fixture
     def storage_cache(self, cache_root, fill_cache):
@@ -640,14 +627,14 @@ class Test_StorageCache:
     def test__cache_hive(self, storage_cache_empty, STOREFORMATVERSION):
         assert storage_cache_empty._cache_hive == STOREFORMATVERSION
 
-    def test_cache_root(self, storage_cache_empty, cache_root_empty):
-        cache_root_expect = str(cache_root_empty)
+    def test_cache_root(self, storage_cache_empty, cache_root):
+        cache_root_expect = str(cache_root)
         assert storage_cache_empty.cache_root == cache_root_expect
 
     def test__cache_path(
-        self, storage_cache_empty, cache_root_empty, STOREFORMATVERSION
+        self, storage_cache_empty, cache_root, STOREFORMATVERSION
     ):
-        cache_path_expect = str(cache_root_empty / STOREFORMATVERSION)
+        cache_path_expect = str(cache_root / STOREFORMATVERSION)
         assert storage_cache_empty._cache_path == cache_path_expect
 
     def test_reset_cache(self, storage_cache, cache_root):
