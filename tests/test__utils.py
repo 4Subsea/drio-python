40b8eada07104016970bcaa43b4dfce20b8777e2
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-04 10:05:09 +0200

    refactor tests

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -6,8 +6,8 @@ from datareservoirio._utils import DataHandler
 
 class Test_DataHandler:
 
-    def test__init__(self):
-
+    @pytest.fixture
+    def series(self):
         index_list = (
             1640995215379000000,
             1640995219176000000,
@@ -16,53 +16,23 @@ class Test_DataHandler:
             1640995271472000000,
         )
         values_list = (-0.2, -0.1, 0.2, 0.1, 1.2)
-        series = pd.Series(data=values_list, index=index_list, name="values")
+        return pd.Series(data=values_list, index=index_list, name="values")
 
+    def test__init__(self, series):
         data_handler = DataHandler(series)
         pd.testing.assert_series_equal(data_handler._series, series)
 
-    def test__init__raises_type(self):
-
-        index_list = (
-            1640995215379000000,
-            1640995219176000000,
-            1640995227270000000,
-            1640995267223000000,
-            1640995271472000000,
-        )
-        values_list = (-0.2, -0.1, 0.2, 0.1, 1.2)
-        df = pd.DataFrame(data={"index": index_list, "values": values_list})
-
+    def test__init__raises_type(self, series):
+        df = series.reset_index()
         with pytest.raises(ValueError):
             DataHandler(df)
 
-    def test__init__raises_name(self):
-
-        index_list = (
-            1640995215379000000,
-            1640995219176000000,
-            1640995227270000000,
-            1640995267223000000,
-            1640995271472000000,
-        )
-        values_list = (-0.2, -0.1, 0.2, 0.1, 1.2)
-        series = pd.Series(data=values_list, index=index_list, name="invalid_name")
-
+    def test__init__raises_name(self, series):
+        series.name = "invalid-name"
         with pytest.raises(ValueError):
             DataHandler(series)
 
-    def test__init__raises_index_name(self):
-
-        index_list = (
-            1640995215379000000,
-            1640995219176000000,
-            1640995227270000000,
-            1640995267223000000,
-            1640995271472000000,
-        )
-        values_list = (-0.2, -0.1, 0.2, 0.1, 1.2)
-        series = pd.Series(data=values_list, index=index_list, name="values")
-        series.index.name = "invalid_name"
-
+    def test__init__raises_index_name(self, series):
+        series.index.name = "invalid-name"
         with pytest.raises(ValueError):
             DataHandler(series)
