52196c5db4c927ba015aa0bdc4adb6b01f059e00
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-09 14:00:03 +0200

    simplify tests

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -48,24 +48,22 @@ class Test_TokenCache:
     @pytest.mark.parametrize("session_key", [None, "foobar"])
     def test__init__(self, session_key):
         token_cache = TokenCache(session_key=session_key)
-
         assert os.path.exists(token_cache._token_root)
 
     def test__token_root(self, token_cache, tmp_path):
         root_out = token_cache._token_root
         assert os.path.exists(root_out)
-        assert os.path.split(root_out)[-1] == "datareservoirio"
         assert root_out == str(tmp_path / "datareservoirio")
 
-    def test_token_path(self, token_cache):
+    def test_token_path(self, token_cache, token_root):
         path_out = token_cache.token_path
-        path_expect = os.path.join(token_cache._token_root, "token.PROD")
+        path_expect = os.path.join(token_root, "token.PROD")
         assert path_out == path_expect
 
-    def test_token_path_with_session_key(self):
+    def test_token_path_with_session_key(self, token_root):
         token_cache = TokenCache(session_key="foobar")
         path_out = token_cache.token_path
-        path_expect = os.path.join(token_cache._token_root, "token.PROD.foobar")
+        path_expect = os.path.join(token_root, "token.PROD.foobar")
         assert path_out == path_expect
 
     def test_token_none(self, token_cache):
