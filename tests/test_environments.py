f49b7b82f18224fe2d2c3aeeee5ea72eeb354250
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-09 15:59:14 +0200

    simplify tests by using _onstants

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,5 +1,6 @@
 import pytest
 
+from datareservoirio import _constants
 from datareservoirio.environments import Environment
 
 
@@ -13,11 +14,11 @@ class Test_Environment:
     def test__init__(self):
         environment = Environment()
         assert environment.current_environment == "PROD"
-        assert environment.api_base_url == "https://reservoir-api.4subsea.net/api/"
+        assert environment.api_base_url == _constants.API_BASE_URL_PROD
 
     def test_api_base_url(self, environment):
         base_url_out = environment.api_base_url
-        base_url_expect = "https://reservoir-api.4subsea.net/api/"
+        base_url_expect = _constants.API_BASE_URL_PROD
         assert base_url_out == base_url_expect
 
     def test_current_environment(self, environment):
@@ -33,11 +34,11 @@ class Test_Environment:
     def test_set_qa(self, environment):
         environment.set_qa()
         assert environment.current_environment == "QA"
-        assert environment.api_base_url == "https://reservoir-api-qa.4subsea.net/api/"
-        assert environment._application_insight_connectionstring == "InstrumentationKey=aec779fc-7a4c-4580-b205-cd9f8ecdcf48;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
+        assert environment.api_base_url == _constants.API_BASE_URL_QA
+        assert environment._application_insight_connectionstring == _constants.APPLICATIONINSIGHTS_QA_CONNECTIONSTRING
 
     def test_set_test(self, environment):
         environment.set_test()
         assert environment.current_environment == "TEST"
-        assert environment.api_base_url == "https://reservoir-api-test.4subsea.net/api/"
-        assert environment._application_insight_connectionstring == "InstrumentationKey=725af0e5-7530-4f2c-b055-36258831785e;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"
+        assert environment.api_base_url == _constants.API_BASE_URL_TEST
+        assert environment._application_insight_connectionstring == _constants.APPLICATIONINSIGHTS_TEST_CONNECTIONSTRING
