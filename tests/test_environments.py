fe0dd195507029f12f381ba5649191329414e1ab
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-09 15:46:19 +0200

    test api_base_url

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,8 +1,21 @@
+import pytest
+
 from datareservoirio.environments import Environment
 
 
 class Test_Environment:
+
+    @pytest.fixture
+    def environment(self):
+        environment = Environment()
+        return environment
+
     def test__init__(self):
-        env = Environment()
-        assert env.current_environment == "PROD"
-        assert env.api_base_url == "https://reservoir-api.4subsea.net/api/"
+        environment = Environment()
+        assert environment.current_environment == "PROD"
+        assert environment.api_base_url == "https://reservoir-api.4subsea.net/api/"
+
+    def test_api_base_url(self, environment):
+        base_url_out = environment.api_base_url
+        base_url_expect = "https://reservoir-api.4subsea.net/api/"
+        assert base_url_out == base_url_expect
