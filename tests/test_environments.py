201c07deaf6a92a590bb730b2105548a6b17edc3
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-09 15:48:35 +0200

    test current_environment

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -19,3 +19,8 @@ class Test_Environment:
         base_url_out = environment.api_base_url
         base_url_expect = "https://reservoir-api.4subsea.net/api/"
         assert base_url_out == base_url_expect
+
+    def test_current_environment(self, environment):
+        current_env_out = environment.current_environment
+        current_env_expect = "PROD"
+        assert current_env_out == current_env_expect
