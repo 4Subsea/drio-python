8674367887139d9e222e55e9cd5251ea3d72dc61
Vegard Rørvik Solum <vegard.solum@4subsea.com>
2023-05-09 13:31:44 +0200

    test token

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -1,10 +1,27 @@
 import pytest
 import os
+import shutil
+from pathlib import Path
 
 from datareservoirio.authenticate import TokenCache
 
 
+TEST_PATH = Path(__file__).parent
+
+
 class Test_TokenCache:
+
+    @pytest.fixture
+    def token_root(self, tmp_path):
+        return tmp_path / "datareservoirio"
+
+    @pytest.fixture
+    def copytokens(self, token_root):
+        """Copy tokens from source to ``token_root``"""
+        src = TEST_PATH / "testdata" / "tokens"
+        token_root.mkdir(exist_ok=True)
+        for file_i in src.iterdir():
+            shutil.copyfile(file_i, token_root / file_i.name)
 
     @pytest.fixture(autouse=True)
     def mock_user_data_dir(self, monkeypatch, tmp_path):
@@ -15,6 +32,13 @@ class Test_TokenCache:
 
     @pytest.fixture
     def token_cache(self):
+        """TokenCache instance (without token)"""
+        token_cache = TokenCache()
+        return token_cache
+
+    @pytest.fixture
+    def token_cache2(self, copytokens):
+        """TokenCache instance (with token)"""
         token_cache = TokenCache()
         return token_cache
 
@@ -40,3 +64,16 @@ class Test_TokenCache:
         path_out = token_cache.token_path
         path_expect = os.path.join(token_cache._token_root, "token.PROD.foobar")
         assert path_out == path_expect
+
+    def test_token_none(self, token_cache):
+        token_out = token_cache.token
+        assert token_out is None
+
+    def test_token_exists(self, token_cache2):
+        token_out = token_cache2.token
+        assert token_out is not None
+
+    def test_token_exists_session_key(self, copytokens):
+        token_cache = TokenCache(session_key="foobar")
+        token_out = token_cache.token
+        assert token_out is not None
