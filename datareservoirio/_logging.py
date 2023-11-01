b7692e2aba8ece23fe8cb3d637def47ba8bf71f2
Jonathan-Prestmoen-4ss <jonathan.prestmoen@4subsea.com>
2023-11-01 13:27:40 +0100

    include engineroom app id env var and tests

--- datareservoirio/_logging.py
+++ datareservoirio/_logging.py
@@ -6,17 +6,15 @@ from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 import datareservoirio as drio
 
-from ._constants import ENV_VAR_ENABLE_APP_INSIGHTS
+from ._constants import ENV_VAR_ENABLE_APP_INSIGHTS, ENV_VAR_ENGINE_ROOM_APP_ID
 from .globalsettings import environment
-
-ENGINE_ROOM_APP_ID = os.environ["ENGINE_ROOM_APP_ID"]
 
 exceptions_logger = logging.getLogger(__name__ + "_exception_logger")
 exceptions_logger.setLevel(logging.INFO)
 
 if os.getenv(ENV_VAR_ENABLE_APP_INSIGHTS) is not None:
-    env_var_lowered = os.environ[ENV_VAR_ENABLE_APP_INSIGHTS].lower()
-    if env_var_lowered == "true" or env_var_lowered == "1":
+    enable_app_insights = os.environ[ENV_VAR_ENABLE_APP_INSIGHTS].lower()
+    if enable_app_insights == "true" or enable_app_insights == "1":
         exceptions_logger.addHandler(
             AzureLogHandler(
                 connection_string=environment._application_insight_connectionstring
@@ -30,11 +28,12 @@ def log_exception(func):
         try:
             return func(self, *args, **kwargs)
         except Exception as e:
-            if ENGINE_ROOM_APP_ID:
+            if os.getenv(ENV_VAR_ENGINE_ROOM_APP_ID) is not None:
+                engine_room_app_id = os.environ[ENV_VAR_ENGINE_ROOM_APP_ID]
                 properties = {
                     "custom_dimensions": {
                         "drio_package": f"python-datareservoirio/{drio.__version__}",
-                        "EngineRoom App ID": ENGINE_ROOM_APP_ID,
+                        "EngineRoom App ID": engine_room_app_id,
                     }
                 }
             else:
