3712d4a1d6c392b6df2b6a4e51fd5fe46b61c8e4
Heidi Gryteland Holm <54934165+heidi-holm-4ss@users.noreply.github.com>
2023-11-06 09:47:21 +0100

    Add possibility to log retries of client.get() as warnings (#113)

--- datareservoirio/_logging.py
+++ datareservoirio/_logging.py
@@ -10,34 +10,39 @@ from ._constants import ENV_VAR_ENABLE_APP_INSIGHTS, ENV_VAR_ENGINE_ROOM_APP_ID
 from .globalsettings import environment
 
 exceptions_logger = logging.getLogger(__name__ + "_exception_logger")
-exceptions_logger.setLevel(logging.INFO)
+exceptions_logger.setLevel(logging.DEBUG)
 
 if os.getenv(ENV_VAR_ENABLE_APP_INSIGHTS) is not None:
     enable_app_insights = os.environ[ENV_VAR_ENABLE_APP_INSIGHTS].lower()
     if enable_app_insights == "true" or enable_app_insights == "1":
-        exceptions_logger.addHandler(
-            AzureLogHandler(
-                connection_string=environment._application_insight_connectionstring
-            )
+        app_insight_handler = AzureLogHandler(
+            connection_string=environment._application_insight_connectionstring
         )
+        app_insight_handler.setLevel("WARNING")
+        exceptions_logger.addHandler(app_insight_handler)
 
 
-def log_exception(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except Exception as e:
-            properties = {
-                "customDimensions": {
-                    "drioPackage": f"python-datareservoirio/{drio.__version__}"
+def log_decorator(log_level):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except Exception as e:
+                properties = {
+                    "customDimensions": {
+                        "drioPackage": f"python-datareservoirio/{drio.__version__}",
+                    }
                 }
-            }
-            if os.getenv(ENV_VAR_ENGINE_ROOM_APP_ID) is not None:
-                properties["customDimensions"]["engineRoomAppId"] = os.getenv(
-                    ENV_VAR_ENGINE_ROOM_APP_ID
-                )
-            exceptions_logger.exception(e, extra=properties)
-            raise e
+                if os.getenv(ENV_VAR_ENGINE_ROOM_APP_ID) is not None:
+                    properties["customDimensions"]["engineRoomAppId"] = os.getenv(
+                        ENV_VAR_ENGINE_ROOM_APP_ID
+                    )
 
-    return wrapper
+                log_function = getattr(exceptions_logger, log_level)
+                log_function(e, extra=properties)
+                raise e
+
+        return wrapper
+
+    return decorator
--- tests/test__logging.py
+++ tests/test__logging.py
@@ -3,18 +3,19 @@ import types
 
 import pytest
 
-from datareservoirio._logging import exceptions_logger, log_exception
+from datareservoirio._logging import exceptions_logger, log_decorator
 
 
 class my_test_class:
     def __init__(self):
         pass
 
-    @log_exception
+    @log_decorator("exception")
+    @log_decorator("warning")
     def divide_zero(self, num):
         return num / 0
 
-    @log_exception
+    @log_decorator("exception")
     def divide_one(self, num):
         return num / 1
 
@@ -22,14 +23,17 @@ class my_test_class:
 @pytest.fixture
 def my_class():
     my_class = my_test_class()
-    my_class.was_called = False
+    my_class.logging_as_exception = False
+    my_class.logging_as_warning = False
     exceptions_logger.exception = types.MethodType(change_logging, my_class)
+    exceptions_logger.warning = types.MethodType(change_logging_warning, my_class)
+
     return my_class
 
 
 def change_logging(self, msg, *args, exc_info=True, **kwargs):
     if kwargs["extra"]:
-        self.was_called = True
+        self.logging_as_exception = True
         if os.getenv("ENGINE_ROOM_APP_ID") is not None:
             self.engine_room_app_id = kwargs["extra"]["customDimensions"][
                 "engineRoomAppId"
@@ -38,15 +42,29 @@ def change_logging(self, msg, *args, exc_info=True, **kwargs):
         raise ValueError("Missing extra parameters")
 
 
+def change_logging_warning(self, msg, *args, exc_info=True, **kwargs):
+    if kwargs["extra"]:
+        self.logging_as_warning = True
+    else:
+        raise ValueError("Missing extra parameters")
+
+
 def test_divide_zero_is_logged(my_class):
     with pytest.raises(ZeroDivisionError):
         my_class.divide_zero(8)
-    assert my_class.was_called == True
+    assert my_class.logging_as_exception == True
 
 
 def test_divide_one_not_logged(my_class):
     my_class.divide_one(2)
-    assert my_class.was_called == False
+    assert my_class.logging_as_exception == False
+    assert my_class.logging_as_warning == False
+
+
+def test_retries_is_loggged_with_criticality_2(my_class):
+    with pytest.raises(ZeroDivisionError):
+        my_class.divide_zero(8)
+    assert my_class.logging_as_warning == True
 
 
 def test_engine_room_app_id_is_logged(my_class, monkeypatch):
