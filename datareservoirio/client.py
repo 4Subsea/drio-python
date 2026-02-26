44f736ff617aeb4a72786a52ad2fc228f2c5338f
Wojciech Bochniarz <wojciech.bochniarz@4subsea.com>
2026-02-26 15:37:15 +0100

    Fix dataframe slicing when start or end are not provided

--- datareservoirio/client.py
+++ datareservoirio/client.py
@@ -395,6 +395,9 @@ class Client:
         pandas.Series
             Series data
         """
+        start_provided = start is not None
+        end_provided = end is not None
+
         if not start:
             start = _START_DEFAULT
         if not end:
@@ -430,21 +433,19 @@ class Client:
         else:
             df = pd.DataFrame(columns=("index", "values")).astype({"index": "int64"})
 
-        try:
-            series = (
-                df.set_index("index").squeeze("columns").loc[start:end].copy(deep=True)
-            )
-        except KeyError as e:
+        s = df.set_index("index").squeeze("columns")
+
+        # Ensure sorted (cheap if already sorted)
+        if not s.index.is_monotonic_increasing:
             logging.warning(
                 "The time series you requested is not properly ordered. The data will be sorted to attempt to resolve the issue. Please note that this operation may take some time."
             )
-            series = (
-                df.set_index("index")
-                .sort_index()
-                .squeeze("columns")
-                .loc[start:end]
-                .copy(deep=True)
-            )
+            s = s.sort_index()
+
+        series = s.loc[
+            start if start_provided else None : end if end_provided else None
+        ].copy(deep=True)
+
         series.index.name = None
 
         if series.empty and raise_empty:  # may become empty after slicing
